c2a319da26d586f0ceab43440bfcd58fe552e5d7
Unknown <dani_mrx93@hotmail.com>
2018-06-05 23:00:26 -0300

    Se agrega script Bullet y de Enemigo

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public const int speed = 6;
+    public int dirY;
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        CheckBoundaries();
+	}
+
+    private void CheckBoundaries()
+    {
+        Vector3 screenPos = Camera.main.WorldToViewportPoint(transform.position);
+        if (screenPos.x < 0 || screenPos.y < 0 || screenPos.x > 1 || screenPos.y > 1)
+            Destroy(gameObject);
+    }
+
+    private void Movement()
+    {
+        Vector3 movement = Vector3.zero;
+        movement.y = dirY * speed;
+        transform.position += movement * Time.deltaTime;
+    }
+
+    public void SetDirectionY(int val)
+    {
+        dirY = val;
+    }
+}
--- Assets/Script/PlayerAirplane.cs
+++ Assets/Script/PlayerAirplane.cs
@@ -1,16 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAirplane : MonoBehaviour {
+public class PlayerAirplane : MonoBehaviour
+{
+    public int speed = 6;
+    public float xMin, xMax, yMin, yMax;
+    public GameObject bulletPrefab;
+    public float shootingTime;
+    public bool canShoot = true;
+    private const float resetShootTime = 0.1f;
+    private Vector3 movement = Vector3.zero;
+    private GameObject firstCanon;
+    // Use this for initialization
+    void Start()
+    {
+        firstCanon = GameObject.Find("FirstCanon");
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shoot();
+        CheckCanShoot();
+    }
+
+    private void CheckCanShoot()
+    {
+        if (!canShoot)
+            shootingTime += Time.deltaTime;
+        if (shootingTime >= resetShootTime)
+        {
+            canShoot = true;
+            shootingTime = 0;
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKey(KeyCode.Space) && canShoot)
+        {
+            canShoot = false;
+            GameObject b = Instantiate(bulletPrefab, firstCanon.transform.position, transform.rotation, transform.parent);
+            b.GetComponent<Bullet>().SetDirectionY(1);
+        }
+    }
+
+    private void Movement()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        Vector3 newPos = transform.position + movement * speed * Time.deltaTime;
+        newPos.x = Mathf.Clamp(newPos.x, xMin, xMax);
+        newPos.y = Mathf.Clamp(newPos.y, yMin, yMax);
+        transform.position = newPos;
+    }
 }
