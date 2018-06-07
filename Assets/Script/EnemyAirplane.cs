cbd1ec79712b116fc822bf7f3cbd8f52bbf32cab
Unknown <dani_mrx93@hotmail.com>
2018-06-06 22:44:39 -0300

    Se mejora el movimiento de la nave enemiga

--- Assets/Script/EnemyAirplane.cs
+++ Assets/Script/EnemyAirplane.cs
@@ -2,19 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAirplane : MonoBehaviour {
+public class EnemyAirplane : MonoBehaviour
+{
+    const int speed = 4;
+    public Vector3 direction;
+    public int minDistance = 3;
+    private GameObject player;
+    public bool isChasing = true;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameManager.Get().player;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
+    }
+
+    private void Movement()
+    {
+        Vector3 movement = Vector3.zero;
+        float distanceToPlayer = Mathf.Abs(player.transform.position.y - transform.position.y);
+        if (distanceToPlayer > minDistance && isChasing)
+            direction = (player.transform.position - transform.position).normalized;
+        else
+            isChasing = false;
+        movement = direction * speed * Time.deltaTime;
+        transform.position += movement;
     }
 }
