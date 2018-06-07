9d2c3a75955224ebfc6766559adaee05aeb44397
Unknown <dani_mrx93@hotmail.com>
2018-06-06 22:44:14 -0300

    Se agrega GameManager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    private int score = 0;
+    public GameObject player;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() {
+
+    }
+
+    public static GameManager Get()
+    {
+        return instance;
+    }
+
+    public void SetScore(int val)
+    {
+        score = val;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
