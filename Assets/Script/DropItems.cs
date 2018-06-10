4477cf850b18b10b3413db1f4da4cee8b36482cd
Unknown <dani_mrx93@hotmail.com>
2018-06-09 23:02:07 -0300

    Se crea script DropItems() que dropea items

---
+++ Assets/Script/DropItems.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItems : MonoBehaviour{
+
+    public List<GameObject> items;
+    public int dropProb;
+
+    public void DropItem(Vector3 pos)
+    {
+        if (Random.Range(1,10) > dropProb)
+        {
+            if (items.Count > 0)
+            {
+                int typeDrop = Random.Range(0, items.Count);
+                Instantiate(items[typeDrop], pos, items[typeDrop].transform.rotation, transform.parent);
+            }
+        }
+    }
+}
