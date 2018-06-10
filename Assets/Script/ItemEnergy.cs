3f952a01e583441a819c1230aba8db30da66b5c7
Unknown <dani_mrx93@hotmail.com>
2018-06-09 23:01:41 -0300

    Se crea item Energy

---
+++ Assets/Script/ItemEnergy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemEnergy : MonoBehaviour {
+
+    public const int quantity = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerAirplane>().energy++;
+            Destroy(gameObject);
+        }
+    }
+
+}
